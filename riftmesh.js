2f07930d3ea0dbc7303207ecadf64f32797d2ef0
Vladimir Vukicevic <vladimir@pobox.com>
2014-04-22 22:23:39 -0400

    Update

--- riftmesh.js
+++ riftmesh.js
@@ -5,13 +5,105 @@ var logtext = null;
 
 var g = {};
 
+var LEFT_EYE = 0, RIGHT_EYE = 1;
+
 function startRendering() {
-  g.scene = new THREE.Scene();
-  g.camera = new THREE.PerspectiveCamera(75, hmd.info.pixelWidth/hmd.info.pixelHeight, 0.1, 1000);
+  g.width = hmd.info.pixelWidth/2;
+  g.height = hmd.info.pixelHeight/2;
+
+  g.distortionCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000);
+  g.distortionCamera.translateZ(1);
+
   g.r = new THREE.WebGLRenderer();
+  g.r.setClearColor(0xccccee);
+  g.r.setSize(g.width, g.height);
 
-  g.r.setSize(hmd.info.pixelWidth, hmd.info.pixelHeight);
+  $('#container').append(g.r.domElement);
 
+  // Man, this really needs a saner way to build this
+  g.distortionGeometries = Array(2);
+  g.viewAdjust = Array(2);
+  g.distortedViewport = Array(2);
+
+  for (var eye = 0; eye < 2; ++eye) {
+    var dm = eye == LEFT_EYE ? hmd.leftEye : hmd.rightEye;
+    var geom = new THREE.BufferGeometry();
+
+    // XXX this is not really ideal; we really want to pack these things all together
+    // instead of using them as separate streams.
+    var idx = geom.addAttribute('index', Uint16Array, dm.distortionIndices.length, 1).array;
+
+    var pos = geom.addAttribute('position', Float32Array, dm.distortionVertices.length, 3).array;
+    var fact = geom.addAttribute('factors', Float32Array, dm.distortionVertices.length, 2).array; // (timewarp, vignette)
+    var texR = geom.addAttribute('texROffset', Float32Array, dm.distortionVertices.length, 2).array;
+    var texG = geom.addAttribute('texGOffset', Float32Array, dm.distortionVertices.length, 2).array;
+    var texB = geom.addAttribute('texBOffset', Float32Array, dm.distortionVertices.length, 2).array;
+
+    geom.hasPositions = true;
+
+    for (var i = 0; i < dm.distortionIndices.length; ++i) {
+      idx[i] = dm.distortionIndices[i];
+    }
+
+    var dv = dm.distortionVertices;
+    for (var i = 0; i < dm.distortionVertices.length; ++i) {
+      pos[i*3+0] = dv[i].pos[0];
+      pos[i*3+1] = dv[i].pos[1];
+      pos[i*3+2] = 0;
+
+      fact[i*2+0] = dv[i].timeWarpFactor;
+      fact[i*2+1] = dv[i].vignetteFactor;
+
+      texR[i*2+0] = dv[i].texR[0];
+      texR[i*2+1] = dv[i].texR[1];
+      texG[i*2+0] = dv[i].texG[0];
+      texG[i*2+1] = dv[i].texG[1];
+      texB[i*2+0] = dv[i].texB[0];
+      texB[i*2+1] = dv[i].texB[1];
+    }
+
+    geom.offsets = [{ start: 0, index: 0, count: dm.distortionIndices.length}];
+
+    geom.computeBoundingSphere();
+    geom.computeBoundingBox();
+
+    log("bs", geom.boundingSphere.radius);
+    log("bbox",
+        "[" + geom.boundingBox.min.x +","+ geom.boundingBox.min.y +","+ geom.boundingBox.min.z + "]",
+        "[" + geom.boundingBox.max.x +","+ geom.boundingBox.max.y +","+ geom.boundingBox.max.z + "]");
+
+    g.distortionGeometries[eye] = geom;
+    g.viewAdjust[eye] = new THREE.Vector3(dm.viewAdjust[0], dm.viewAdjust[1], dm.viewAdjust[2]);
+    g.distortedViewport[eye] = dm.distortedViewport;
+  }
+
+  var mat = new THREE.MeshBasicMaterial({color: 0x111111, side: THREE.FrontSide});
+
+  g.scenes = Array(2);
+  g.scenes[0] = new THREE.Scene();
+  g.scenes[0].add(new THREE.AmbientLight(0x444444));
+  g.scenes[0].add(new THREE.Mesh(g.distortionGeometries[0], mat));
+  g.scenes[0].add(new THREE.Mesh(new THREE.CubeGeometry(1, 1, 0.0),
+                                 new THREE.MeshBasicMaterial({color: 0xff1111, wireframe: true})));
+
+  g.scenes[1] = new THREE.Scene();
+  g.scenes[1].add(new THREE.AmbientLight(0x444444));
+  g.scenes[1].add(new THREE.Mesh(g.distortionGeometries[1], mat));
+  g.scenes[1].add(new THREE.Mesh(new THREE.CubeGeometry(1, 1, 0.0),
+                                 new THREE.MeshBasicMaterial({color: 0xff1111, wireframe: true})));
+
+  function redraw() {
+    requestAnimationFrame(redraw);
+
+    for (var eye = 0; eye < 2; eye++) {
+      g.r.setViewport(eye * (g.width / 2), 0,
+                      g.width / 2, g.height);
+      g.r.setClearColor(eye ? 0xccccee : 0xeecccc);
+      g.r.render(g.scenes[eye], g.distortionCamera);
+    }
+  }
+
+  redraw();
 }
 
 function hasDevs(vrdevs) {
