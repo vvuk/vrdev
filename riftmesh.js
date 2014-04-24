c2d04b16f156faddf1f5dced48ba65cf283e018d
Vladimir Vukicevic <vladimir@pobox.com>
2014-04-23 23:52:47 -0400

    updates

--- riftmesh.js
+++ riftmesh.js
@@ -7,12 +7,69 @@ var g = {};
 
 var LEFT_EYE = 0, RIGHT_EYE = 1;
 
-function startRendering() {
-  g.width = hmd.info.pixelWidth/2;
-  g.height = hmd.info.pixelHeight/2;
 
-  g.distortionCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000);
-  g.distortionCamera.translateZ(1);
+// these are reimplementations of the Oculus SDK functions;
+// See the SDK for more information (OVR_CAPI.cpp, OVR_Stereo.cpp)
+function FovToNDCScaleOffset(fov)
+{
+  var pxscale = 2.0 / (fov.leftTan + fov.rightTan);
+  var pxoffset = (fov.leftTan - fov.rightTan) * pxscale * 0.5;
+  var pyscale = 2.0 / (fov.upTan + fov.downTan);
+  var pyoffset = (fov.upTan - fov.downTan) * pyscale * 0.5;
+
+  return { scale: [pxscale, pyscale], offset: [pxoffset, pyoffset] };
+}
+
+function FovToProjection(fov, rightHanded /* = true */, zNear /* = 0.01 */, zFar /* = 10000.0 */)
+{
+  rightHanded = rightHanded === undefined ? true : rightHanded;
+  zNear = zNear === undefined ? 0.01 : zNear;
+  zFar = zFar === undefined ? 10000.0 : zFar;
+
+  var handednessScale = rightHanded ? -1.0 : 1.0;
+
+  // start with an identity matrix
+  var mobj = new THREE.Matrix4();
+  var m = mobj.elements;
+
+  // and with scale/offset info for normalized device coords
+  var scaleAndOffset = FovToNDCScaleOffset(fov);
+
+  // X result, map clip edges to [-w,+w]
+  m[0*4+0] = scaleAndOffset.scale[0];
+  m[0*4+1] = 0.0;
+  m[0*4+2] = scaleAndOffset.offset[0] * handednessScale;
+  m[0*4+3] = 0.0;
+
+  // Y result, map clip edges to [-w,+w]
+  // Y offset is negated because this proj matrix transforms from world coords with Y=up,
+  // but the NDC scaling has Y=down (thanks D3D?)
+  m[1*4+0] = 0.0;
+  m[1*4+1] = scaleAndOffset.scale[1];
+  m[1*4+2] = -scaleAndOffset.offset[1] * handednessScale;
+  m[1*4+3] = 0.0;
+
+  // Z result (up to the app)
+  m[2*4+0] = 0.0;
+  m[2*4+1] = 0.0;
+  m[2*4+2] = zFar / (zNear - zFar) * -handednessScale;
+  m[2*4+3] = (zFar * zNear) / (zNear - zFar);
+
+  // W result (= Z in)
+  m[3*4+0] = 0.0;
+  m[3*4+1] = 0.0;
+  m[3*4+2] = handednessScale;
+  m[3*4+3] = 0.0;
+
+  mobj.transpose();
+
+  log(m.toSource());
+  return mobj;
+}
+
+function startRendering() {
+  g.width = hmd.info.pixelWidth;
+  g.height = hmd.info.pixelHeight;
 
   g.r = new THREE.WebGLRenderer();
   g.r.setClearColor(0xccccee);
@@ -26,18 +83,18 @@ function startRendering() {
   g.distortedViewport = Array(2);
 
   for (var eye = 0; eye < 2; ++eye) {
-    var dm = eye == LEFT_EYE ? hmd.leftEye : hmd.rightEye;
+    var dm = (eye == LEFT_EYE) ? hmd.leftEye : hmd.rightEye;
     var geom = new THREE.BufferGeometry();
 
     // XXX this is not really ideal; we really want to pack these things all together
     // instead of using them as separate streams.
-    var idx = geom.addAttribute('index', Uint16Array, dm.distortionIndices.length, 1).array;
+    var idx = geom.addAttribute("index", Uint16Array, dm.distortionIndices.length, 1).array;
 
-    var pos = geom.addAttribute('position', Float32Array, dm.distortionVertices.length, 3).array;
-    var fact = geom.addAttribute('factors', Float32Array, dm.distortionVertices.length, 2).array; // (timewarp, vignette)
-    var texR = geom.addAttribute('texROffset', Float32Array, dm.distortionVertices.length, 2).array;
-    var texG = geom.addAttribute('texGOffset', Float32Array, dm.distortionVertices.length, 2).array;
-    var texB = geom.addAttribute('texBOffset', Float32Array, dm.distortionVertices.length, 2).array;
+    var pos = geom.addAttribute("position", Float32Array, dm.distortionVertices.length, 3).array;
+    var fact = geom.addAttribute("aPackedData", Float32Array, dm.distortionVertices.length, 2).array; // (timewarp, vignette)
+    var texR = geom.addAttribute("aTexCoordR", Float32Array, dm.distortionVertices.length, 2).array;
+    var texG = geom.addAttribute("aTexCoordG", Float32Array, dm.distortionVertices.length, 2).array;
+    var texB = geom.addAttribute("aTexCoordB", Float32Array, dm.distortionVertices.length, 2).array;
 
     geom.hasPositions = true;
 
@@ -75,32 +132,126 @@ function startRendering() {
     g.distortionGeometries[eye] = geom;
     g.viewAdjust[eye] = new THREE.Vector3(dm.viewAdjust[0], dm.viewAdjust[1], dm.viewAdjust[2]);
     g.distortedViewport[eye] = dm.distortedViewport;
+
+    log("viewport", g.distortedViewport[eye].toSource());
   }
 
-  var mat = new THREE.MeshBasicMaterial({color: 0x111111, side: THREE.FrontSide});
 
-  g.scenes = Array(2);
-  g.scenes[0] = new THREE.Scene();
-  g.scenes[0].add(new THREE.AmbientLight(0x444444));
-  g.scenes[0].add(new THREE.Mesh(g.distortionGeometries[0], mat));
-  g.scenes[0].add(new THREE.Mesh(new THREE.CubeGeometry(1, 1, 0.0),
-                                 new THREE.MeshBasicMaterial({color: 0xff1111, wireframe: true})));
+  g.renderTargets = Array(2);
+  g.renderTargets[0] = new THREE.WebGLRenderTarget(g.width / 2, g.height, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat });
+  g.renderTargets[1] = new THREE.WebGLRenderTarget(g.width / 2, g.height, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat });
 
-  g.scenes[1] = new THREE.Scene();
-  g.scenes[1].add(new THREE.AmbientLight(0x444444));
-  g.scenes[1].add(new THREE.Mesh(g.distortionGeometries[1], mat));
-  g.scenes[1].add(new THREE.Mesh(new THREE.CubeGeometry(1, 1, 0.0),
-                                 new THREE.MeshBasicMaterial({color: 0xff1111, wireframe: true})));
+  if (!hmd.leftEye.uvScale) {
+    hmd.leftEye.uvScale = [0.32385197281837463, 0.23383478820323944];
+    hmd.leftEye.uvOffset = [0.6856241226196289, 0.5];
+    hmd.rightEye.uvScale = [0.32385197281837463, 0.23383478820323944];
+    hmd.rightEye.uvOffset = [0.3143759071826935, 0.5];
+  }
+
+  g.distortionShaderMaterials = Array(2);
+  g.distortionShaderMaterials[0] = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById("distortion_vs").textContent,
+    fragmentShader: document.getElementById("distortion_fs").textContent,
+    depthTest: false,
+    side: THREE.DoubleSide,
+
+    attributes: {
+      aPackedData: { type: "v4" },
+      aTexCoordR: { tyoe: "v2" },
+      aTexCoordG: { tyoe: "v2" },
+      aTexCoordB: { tyoe: "v2" }
+    },
+
+    uniforms: {
+      uEyeToSourceUVScale: { type: "v2", value: new THREE.Vector2(hmd.leftEye.uvScale[0], hmd.leftEye.uvScale[1]) },
+      uEyeToSourceUVOffset: { type: "v2", value: new THREE.Vector2(hmd.leftEye.uvOffset[0], hmd.leftEye.uvOffset[1]) },
+      uTexture: { type: "t", value: g.renderTargets[0] }
+    }
+  });
+  g.distortionShaderMaterials[1] = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById("distortion_vs").textContent,
+    fragmentShader: document.getElementById("distortion_fs").textContent,
+    depthTest: false,
+    side: THREE.DoubleSide,
+
+    attributes: {
+      aPackedData: { type: "v4" },
+      aTexCoordR: { tyoe: "v2" },
+      aTexCoordG: { tyoe: "v2" },
+      aTexCoordB: { tyoe: "v2" }
+    },
+
+    uniforms: {
+      uEyeToSourceUVScale: { type: "v2", value: new THREE.Vector2(hmd.rightEye.uvScale[0], hmd.rightEye.uvScale[1]) },
+      uEyeToSourceUVOffset: { type: "v2", value: new THREE.Vector2(hmd.rightEye.uvOffset[0], hmd.rightEye.uvOffset[1]) },
+      uTexture: { type: "t", value: g.renderTargets[1] }
+    }
+  });
+
+  {
+    // the scene that takes care of drawing the two eyes using distortion matrices
+    g.distortionCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10000);
+    g.distortionCamera.translateZ(1);
+
+    g.distortionScene = new THREE.Scene();
+    g.distortionScene.add(new THREE.Mesh(g.distortionGeometries[0], g.distortionShaderMaterials[0]));
+    g.distortionScene.add(new THREE.Mesh(g.distortionGeometries[1], g.distortionShaderMaterials[1]));
+  }
+
+  {
+    // debug camera & scene; just draws undistorted views side by side
+    if (false) {
+      g.debugCamera = new THREE.OrthographicCamera(-g.width/2 - 10, g.width/2 + 10, g.height/2 + 10, -g.height/2 -10, 0.1, 10000);
+      g.debugCamera.translateZ(100);
+      var planeLeft = new THREE.Mesh(new THREE.PlaneGeometry(g.width / 2, g.height), new THREE.MeshBasicMaterial({ color: 0x00ff00, map: g.renderTargets[0] }))
+      planeLeft.translateX(-g.width/4);
+      var planeRight = new THREE.Mesh(new THREE.PlaneGeometry(g.width / 2, g.height), new THREE.MeshBasicMaterial({ color: 0x0000ff, map: g.renderTargets[1] }))
+      planeRight.translateX(g.width/4);
+    }
+
+    g.debugCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10000);
+    g.debugCamera.translateZ(1);
+    var planeLeft = new THREE.Mesh(new THREE.PlaneGeometry(1, 2), new THREE.MeshBasicMaterial({ color: 0x00ff00, map: g.renderTargets[0] }))
+    planeLeft.translateX(-0.5);
+    var planeRight = new THREE.Mesh(new THREE.PlaneGeometry(1, 2), new THREE.MeshBasicMaterial({ color: 0x0000ff, map: g.renderTargets[1] }))
+    planeRight.translateX(0.5);
+
+    g.debugScene = new THREE.Scene();
+    g.debugScene.add(planeLeft);
+    g.debugScene.add(planeRight);
+  }
+
+  {
+    // the actual content scene
+    g.mainScene = new THREE.Scene();
+    g.mainScene.add(new THREE.AmbientLight(0x44aa44));
+    //g.mainScene.add(new THREE.Mesh(new THREE.TorusGeometry(10, 25, 15, 30), new THREE.MeshBasicMaterial({color: 0xff1111, wireframe: true})));
+    g.mainScene.add(new THREE.Mesh(new THREE.CubeGeometry(10, 10, 10), new THREE.MeshBasicMaterial({color: 0xff1111, wireframe: true})));
+  }
+
+  g.eyeCameras = Array(2);
+  g.eyeCameras[0] = new THREE.Camera();
+  g.eyeCameras[1] = new THREE.Camera();
+
+  g.eyeCameras[0].projectionMatrix = FovToProjection(hmd.info.recommendedEyeFieldOfView[0]);
+  g.eyeCameras[1].projectionMatrix = FovToProjection(hmd.info.recommendedEyeFieldOfView[1]);
+
+  g.eyeCameras[0].translateZ(25);
+  g.eyeCameras[1].translateZ(25);
 
   function redraw() {
     requestAnimationFrame(redraw);
 
     for (var eye = 0; eye < 2; eye++) {
-      g.r.setViewport(eye * (g.width / 2), 0,
-                      g.width / 2, g.height);
+      //g.eyeCameras[eye].setRotationFromQuaternion(...);
       g.r.setClearColor(eye ? 0xccccee : 0xeecccc);
-      g.r.render(g.scenes[eye], g.distortionCamera);
+      g.r.render(g.mainScene, g.eyeCameras[eye], g.renderTargets[eye], true);
     }
+
+    g.r.setClearColor(0);
+    g.r.render(g.distortionScene, g.distortionCamera);
+    //g.r.render(g.debugScene, g.debugCamera);
+    //g.r.render(g.mainScene, g.eyeCameras[1]);
   }
 
   redraw();
@@ -131,6 +282,21 @@ function hasDevs(vrdevs) {
     }
   }
 
+  if (false) {
+    log("====== mock.js ======");
+    var mockOutHMD = { id: "mockjs0", deviceType: "hmd", deviceName: "MockJS", info: hmd.info, leftEye: hmd.leftEye, rightEye: hmd.rightEye };
+    var mockOutPos = { id: "mockjs0", deviceType: "position", deviceName: "MockJS" };
+    log("var mockHMD = ");
+    log(mockOutHMD.toSource());
+    log(";");
+    log("var mockPosition = ");
+    log(mockOutPos.toSource());
+    log(";");
+    log("mockHMD.setEyeFov = function() { alert('setEyeFov not supported in mockHMD'); };");
+    log("mockPosition.getState = function(timeOffset, fvals) { for (var i = 0; i < 19; ++i) { fvals[i] = 0.0; } return 19; };");
+    return;
+  }
+
   if (hmd && posdev) {
     startRendering();
   }
@@ -140,7 +306,7 @@ $(function() {
   logtext = $("#logtext")[0];
   logtext.textContent = "";
 
-  if (false) {
+  if (true) {
     navigator.mozGetVRDevices(hasDevs);
   } else {
     hasDevs([mockHMD, mockPosition]);
